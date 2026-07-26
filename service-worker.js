35c2ef721547dfb2f9a185428fb903f30c4380f2
nicholaspresgrave <nicholas.presgrave@gmail.com>
2026-07-26 21:30:06 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
-const CACHE='neuro-note-v40'; const FILES=['./','./index.html','./styles.css','./sensory-map.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='neuroexam-public-v58'; const FILES=['./','./index.html','./styles.css','./sensory-map.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached=>cached || fetch(event.request))));
